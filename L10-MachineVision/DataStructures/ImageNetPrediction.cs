4963f27d9ab72f7f4f9795a616ad79d450965c00
Marko_ Kvyk <markomar2000@gmail.com>
2021-01-09 19:34:53 +0200

    LB 10 - machine vision

---
+++ L10-MachineVision/DataStructures/ImageNetPrediction.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Data;
+
+namespace L10_MachineVision.DataStructures
+{
+    public class ImageNetPrediction
+    {
+        [ColumnName("grid")]
+        public float[] PredictedLabels;
+    }
+}
